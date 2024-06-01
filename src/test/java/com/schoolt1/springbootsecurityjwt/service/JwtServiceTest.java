1fd7ca972e266a61f629c5b5d706baf9d17d2cde
Anton Worotyncev <anto.vorotynvcev@gmail.com>
2024-06-01 03:13:47 +0300

    Добавлен тест на jwtServiceTest

--- src/test/java/com/schoolt1/springbootsecurityjwt/service/JwtServiceTest.java
+++ src/test/java/com/schoolt1/springbootsecurityjwt/service/JwtServiceTest.java
@@ -1,44 +1,38 @@
 package com.schoolt1.springbootsecurityjwt.service;
 
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.util.ReflectionTestUtils;
+import com.schoolt1.springbootsecurityjwt.service.impl.JwtServiceImpl;
 
-import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+    public class JwtServiceTest {
 
-class JwtServiceTest {
+    @InjectMocks
+    private JwtServiceImpl jwtService;
 
-    @Test
-    void extractUserName() {
+    @Mock
+    private UserDetails userDetails;
+
+    private final String jwtSigningKey = "53A73E5F1C4E0A2D3B5F2D784E6A1B423D6F247D1F6E5C3A596D635A75327856";
+
+    @BeforeEach
+    public void setUp() {
+        ReflectionTestUtils.setField(jwtService, "jwtSigningKey", jwtSigningKey);
     }
 
     @Test
-    void generateToken() {
-    }
-
-    @Test
-    void isTokenValid() {
-    }
-
-    @Test
-    void extractClaim() {
-    }
-
-    @Test
-    void testGenerateToken() {
-    }
-
-    @Test
-    void isTokenExpired() {
-    }
-
-    @Test
-    void extractExpiration() {
-    }
-
-    @Test
-    void extractAllClaims() {
-    }
-
-    @Test
-    void getSigningKey() {
+    public void testGenerateToken() {
+        Mockito.when(userDetails.getUsername()).thenReturn("testuser");
+        String token = jwtService.generateToken(userDetails);
+        Assertions.assertNotNull(token);
     }
 }
